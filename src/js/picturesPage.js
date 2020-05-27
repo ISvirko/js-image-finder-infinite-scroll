dc66e15e02407c260938383f78fbf15c7d54d0fd
ISvirko <ISvirko@yahoo.com>
2020-05-28 00:13:15 +0300

    hw-13-infinite

---
+++ src/js/picturesPage.js
@@ -0,0 +1,123 @@
+import InfiniteScroll from 'infinite-scroll';
+
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import { success, error, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import { defaults } from '@pnotify/core';
+defaults.delay = 2000;
+
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
+
+import { refs } from './refs.js';
+import pictureItemTpl from '../templates/picture-item.hbs';
+import spinner from './spinner';
+
+refs.searchForm.addEventListener('submit', searchImagesHandler);
+refs.galleryList.addEventListener('click', openLargeImg);
+refs.toTopBtn.addEventListener('click', upDownArrowHandler);
+
+const apiKey = '16727206-7ae7a1f614d6d42142bf6389f';
+
+const infScroll = new InfiniteScroll(refs.galleryList, {
+  responseType: 'text',
+  history: false,
+  path: function () {
+    return `https://cors-anywhere.herokuapp.com/https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.inputValue}&page=${this.pageIndex}&per_page=12&key=${apiKey}`;
+  },
+});
+
+infScroll.inputValue = '';
+spinner.show();
+
+infScroll.on('load', response => {
+  const pictures = JSON.parse(response);
+
+  const markup = pictureItemTpl(pictures.hits);
+  const proxyEl = document.createElement('div');
+  proxyEl.innerHTML = markup;
+
+  const parsedItems = proxyEl.querySelectorAll('.photo-card');
+  infScroll.appendItems(parsedItems);
+  spinner.hide();
+});
+
+infScroll.loadNextPage();
+
+function searchImagesHandler(e) {
+  e.preventDefault();
+  clearGallery();
+
+  const input = refs.searchForm.elements.query;
+  const inputValue = input.value;
+  if (!inputValue) return;
+  infScroll.inputValue = inputValue;
+
+  infScroll.option({
+    path() {
+      return `https://cors-anywhere.herokuapp.com/https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.inputValue}&page=${this.pageIndex}&per_page=12&key=${apiKey}`;
+    },
+  });
+
+  spinner.show();
+
+  infScroll.on('load', response => {
+    spinner.hide();
+    const pictures = JSON.parse(response);
+
+    if (pictures.hits.length < 1) {
+      error('No images matching your query have been found');
+      return;
+    }
+
+    const markup = pictureItemTpl(pictures.hits);
+    const proxyEl = document.createElement('div');
+    proxyEl.innerHTML = markup;
+
+    const parsedItems = proxyEl.querySelectorAll('.photo-card');
+    infScroll.appendItems(parsedItems);
+  });
+
+  infScroll.loadNextPage();
+}
+
+function clearGallery() {
+  refs.galleryList.innerHTML = '';
+}
+
+function openLargeImg(e) {
+  if (e.target.nodeName !== 'IMG') return;
+  const largeImg = e.target.dataset.source;
+  const instance = basicLightbox.create(
+    `<img src='${largeImg} class='largeImg' width='800' height='600'/>'`,
+  );
+  instance.show();
+
+  window.addEventListener('keydown', closeModalHandler);
+
+  function closeModalHandler(e) {
+    if (e.code === 'Escape') {
+      instance.close();
+      window.removeEventListener('keydown', closeModalHandler);
+    }
+  }
+}
+
+function upDownArrowHandler() {
+  refs.toTopBtn.classList.remove('toTopBtn');
+  refs.toTopBtn.classList.add('toBottomBtn');
+
+  if (window.pageYOffset === 0) {
+    refs.toTopBtn.classList.remove('toBottomBtn');
+    refs.toTopBtn.classList.add('toTopBtn');
+  }
+
+  smoothScrollHandler();
+}
+
+function smoothScrollHandler() {
+  window.scrollTo({
+    top: document.documentElement.offsetHeight,
+    behavior: 'smooth',
+  });
+}
---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export const refs = {
+  container: document.querySelector('.container'),
+  searchForm: document.forms.searchForm,
+  galleryList: document.querySelector('#galleryList'),
+  loadMoreBtn: document.querySelector('.load-more-btn'),
+  toTopBtn: document.querySelector('.toTopBtn'),
+};
